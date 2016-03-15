c102f861620662d1c23cb2c0cde641883032b869
Ashley E. Stallings <aes421@msstate.edu>
2016-03-14 22:35:54 -0500

    First attempt at image processing painters tape (just from an image found on google)
    Source for code:
    http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/

---
+++ ImageProcessing.py
@@ -0,0 +1,30 @@
+#Source: http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+import numpy as np
+import cv2
+
+#import the image
+image = cv2.imread("PaintersTape.jpg")
+
+#code the colors to recognize
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+
+	
+	([103, 86, 65], [145, 133, 128])
+]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+ 
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv2.inRange(image, lower, upper)
+	output = cv2.bitwise_and(image, image, mask = mask)
+
+	cv2.imshow("images", np.hstack([image, output]))
+	cv2.waitKey(0)
