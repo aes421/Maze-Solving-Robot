0aaeff72e6844fd2b52867cae962376e2392a7e1
Ashley E. Stallings <aes421@msstate.edu>
2016-03-29 19:11:54 -0500

    Finds Contours and places 0 in each cell
    TO-DO: The middle cell has double 0's for some reason
    TO-DO: Create matrix representation

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -9,7 +9,7 @@ def extract_cells(grid):
 	thresh = cv2.adaptiveThreshold(image_gray,255,1,1,11,15)
 
 	#Find countors
-	tempimg, contours ,hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	tempimg, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 	
 	#draw all countours
 	count = 0
@@ -18,11 +18,19 @@ def extract_cells(grid):
 		M = cv2.moments(contours[count])
 		row = int(M['m10']/M['m00'])
 		column = int(M['m01']/M['m00'])
+
 		cv2.putText(grid, "0", (row, column), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
 		cv2.drawContours(grid, contours, count, (255,255,255), 1)
 		count += 1
 	cv2.imshow("test", grid)
 	cv2.waitKey(0)
+	return contours
+
+def create_matrix(c,image):
+	#if color red (from boundaries) if in between c's x,y coordinates
+		#put 0 in matrix
+	#else
+		#put 1 in matrix
 	return
 
 def identify_colors(image):
@@ -31,28 +39,14 @@ def identify_colors(image):
 		# create NumPy arrays from the boundaries
 		lower = np.array(lower, dtype = "uint8")
 		upper = np.array(upper, dtype = "uint8")
-
-		'''#Create adjacency matrix
-		#34 W 42 H
-		col_counter = 0
-		row_counter = 0
-
-	#Source to use: http://www.shogun-toolbox.org/static/notebook/current/Sudoku_recognizer.html
-
-	for row in xrange(110, 435, 34):
-		for column in xrange(110, 520, 42):
-			col_counter += 1
-			cv2.putText(image, "0", (row, column), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0))
-		row_counter += 1'''
  
 	# find the colors within the specified boundaries and apply
 	# the mask
-	mask = cv2.inRange(image, lower, upper)
-	output = cv2.bitwise_and(image, image, mask = mask)
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask = mask)
 
-	#cv2.imshow("images", np.hstack([image, output]))
-	#cv2.imshow("test", image)
-	#cv2.waitKey(0)
+		#cv2.imshow("images", np.hstack([image, output]))
+		#cv2.waitKey(0)
 	return output
 
 
@@ -66,7 +60,7 @@ image = cv2.imread("PaintMaze.png")
 #code the colors to recognize [B, G. R]
 boundaries = [
 	#white
-	([215, 215, 215], [255, 255, 255]),
+	#([215, 215, 215], [255, 255, 255]),
 
 	#red
 	#([0,0,100], [100,100,255]),
@@ -80,7 +74,8 @@ boundaries = [
 ]
 
 grid = identify_colors(image)
-extract_cells(grid)
+c = extract_cells(grid)
+create_matrix(c, image)
 
 
 
