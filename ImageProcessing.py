19ea4fbe6f04f975afc3418d73397799b20358a3
Ashley E. Stallings <aes421@msstate.edu>
2016-04-07 22:36:13 -0500

    Minor changes to make code better.  Output still the same as previous version.

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -2,6 +2,12 @@
 import numpy as np
 import cv2
 
+'''Extreme points 
+leftmost = tuple(cnt[cnt[:,:,0].argmin()][0])
+rightmost = tuple(cnt[cnt[:,:,0].argmax()][0])
+topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
+bottommost = tuple(cnt[cnt[:,:,1].argmax()][0])'''
+
 
 def extract_cells(grid):
 	#convert to gray
@@ -19,6 +25,9 @@ def extract_cells(grid):
 		row = int(M['m10']/M['m00'])
 		column = int(M['m01']/M['m00'])
 
+
+		#output image
+		#print count, ": ", row, column
 		cv2.putText(grid, "0", (row, column), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
 		cv2.drawContours(grid, contours, count, (255,255,255), 1)
 		count += 1
@@ -27,27 +36,49 @@ def extract_cells(grid):
 	return contours
 
 def create_matrix(c,image):
-	#if color red (from boundaries) if in between c's x,y coordinates
+	#if color red (from colorlist) if in between c's x,y coordinates
 		#put 0 in matrix
 	#else
 		#put 1 in matrix
 	return
 
-def identify_colors(image):
-	# loop over the boundaries
-	for (lower, upper) in boundaries:
-		# create NumPy arrays from the boundaries
+
+#This function takes an image and a list of colors, it then individually segments out
+#the specified colors meaning two colors specified will create two different output images.
+#Currently only the last color specified has its iamge returned
+def identify_colors(image, *colors):
+
+	colorlist = []
+	#Add RGB values for each color specified when the function was called
+	#to the list colorlist
+
+	if "blue" in colors:
+		colorlist.append(([115,0,0], [255,100,100]))
+	if "white" in colors:
+		colorlist.append(([215, 215, 215], [255, 255, 255]))
+	if "red" in colors:
+		colorlist.append(([0,0,100], [100,100,255]))
+	if "green" in colors:
+		colorlist.append(([0,115,0], [100,255,100]))
+
+	#loop over the colorlist
+	for (lower, upper) in colorlist:
+		# create NumPy arrays from the colorlist
 		lower = np.array(lower, dtype = "uint8")
 		upper = np.array(upper, dtype = "uint8")
- 
-	# find the colors within the specified boundaries and apply
-	# the mask
+
+
+	
+		#econverts image to b/w with white being anything in the BGR value range
 		mask = cv2.inRange(image, lower, upper)
+		#converts that specified range back to its orginal color
 		output = cv2.bitwise_and(image, image, mask = mask)
 
+		#show the photos side by side
 		#cv2.imshow("images", np.hstack([image, output]))
 		#cv2.waitKey(0)
 	return output
+	
 
 
 
@@ -55,27 +86,12 @@ def identify_colors(image):
 
 
 #import the image
-image = cv2.imread("PaintMaze.png")
+image = cv2.imread("minimaze.png")
 
-#code the colors to recognize [B, G. R]
-boundaries = [
-	#white
-	#([215, 215, 215], [255, 255, 255]),
 
-	#red
-	#([0,0,100], [100,100,255]),
-
-	#blue
-	([115,0,0], [255,100,100]),
-
-	#green
-	#([0,115,0], [100,255,100])
-
-]
-
-grid = identify_colors(image)
+grid = identify_colors(image, "blue")
 c = extract_cells(grid)
-create_matrix(c, image)
+#create_matrix(c, image)
 
 
 
