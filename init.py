72d7f12ac7daab6e5ca1009bee8b47d847fdc21e
EliCDavis <eli.davis1995@gmail.com>
2016-04-27 22:25:51 -0500

    Simulation launches with the generated maze file

--- init.py
+++ init.py
@@ -1,13 +1,13 @@
 import sys
 import Image
 import subprocess
-from ImageProcessing.py import main as imgmain
+from ImageProcessing import main as imgmain
 
-def draw_square(pixels, x, y, w, h):
+def draw_square(pixels, x, y, w, h, c):
 
 	for i in range(w):    # for every pixel:
 	    for j in range(h):
-	        pixels[x+i,y+j] = (50, 50, 50) # set the colour accordingly
+	        pixels[x+i,y+j] = c # set the colour accordingly
 
 def generate_image(matrix, image_name):
 
@@ -21,7 +21,10 @@ def generate_image(matrix, image_name):
 		for col in range(len(matrix[row])):
 
 			if matrix[row][col] == 0:
-				draw_square(pixels, col*square_dimension, row*square_dimension, square_dimension, square_dimension)
+				draw_square(pixels, col*square_dimension, row*square_dimension, square_dimension, square_dimension, (50,50,50))
+
+			if matrix[row][col] == 2:
+				draw_square(pixels, col*square_dimension, row*square_dimension, square_dimension, square_dimension, (0,256,0))
 
 	img.save("launch/"+str(image_name))
 
@@ -45,6 +48,16 @@ def write_world_file(matrix, sim_name):
 	f.write("\nmap\n(\n  name \""+str(sim_name)+"\"\n  size ["+str(matrix_width*2)+" "+str(matrix_height*2)+" 0.5]\n  pose [0 0 0 0]\n  bitmap \""+image_name+"\"\n)\n\n")
 	f.write(second_half)
 
+	# Find the goal in the matrix
+	goal = None
+	for row in range(len(matrix)):
+		for col in range(len(matrix[row])):
+			if matrix[row][col] == 2:
+				goal = (row, col)
+
+	if goal is not None:
+		f.write('block(pose['+str(row)+' '+str(col)+' 5 0] color "green")')
+
 	f.close()
 
 	# Create that image now..
@@ -57,23 +70,14 @@ if __name__ == '__main__':
 		sys.exit("Expecting 2 arguments (Name of Simulation, Maze Image name), but instead recieved "+str(len(sys.argv))+": "+str(sys.argv))
 		
 
+	print(sys.argv)
+
 	name_of_simulation = sys.argv[1]
 	name_of_maze_file = sys.argv[2]
 
 	print("Extracting Information From Maze Image")
 
 	# Grab a matrix using ashley's code
-	sample_matrix = [
-		[0,0,0,1,0,1,1,0],
-		[1,1,1,1,0,0,1,0],
-		[0,1,0,0,0,0,1,0],
-		[0,1,0,2,1,1,1,0],
-		[0,1,1,1,1,0,1,0],
-		[0,0,1,0,0,0,1,1],
-		[0,0,1,1,0,0,0,0],
-		[0,0,1,0,0,0,0,0]
-	]
-
 	sample_matrix = imgmain(name_of_maze_file)
 
 	print("Image successfuly parsed!")
@@ -82,5 +86,5 @@ if __name__ == '__main__':
 	write_world_file(sample_matrix, name_of_simulation)
 
 	print("Starting Ross...")
-	subprocess.call(["roslaunch MazeSolvingRobot sample.launch"], shell=True)
+	subprocess.call(["roslaunch MazeSolvingRobot simulation.launch"], shell=True)
 	print("Ross Started!")
